4fbf7892d21a80c6d08816fe6ac79cf0804251f0
Adrian <adrpieper@gmail.com>
2017-01-18 20:12:53 +0100

    Dodałem 3 fragmenty

--- app/src/main/java/com/sda/pieper/viewpagerapp/MainActivity.java
+++ app/src/main/java/com/sda/pieper/viewpagerapp/MainActivity.java
@@ -1,13 +1,16 @@
 package com.sda.pieper.viewpagerapp;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
 }
