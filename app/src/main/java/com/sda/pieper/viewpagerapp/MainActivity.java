78efd45799d85a3e342f3a540bafed5e802af6fa
Adrian <adrpieper@gmail.com>
2017-01-18 19:55:23 +0100

    Dodałem cały projekt

---
+++ app/src/main/java/com/sda/pieper/viewpagerapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sda.pieper.viewpagerapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
