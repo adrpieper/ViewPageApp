bc0da9597fd87b849b614335832e87da1a825ae7
Adrian <adrpieper@gmail.com>
2017-01-18 20:32:35 +0100

    Dodałem szkielet implementacji ViewPager

--- app/src/main/java/com/sda/pieper/viewpagerapp/MainActivity.java
+++ app/src/main/java/com/sda/pieper/viewpagerapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.sda.pieper.viewpagerapp;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
 public class MainActivity extends Activity {
 
@@ -12,5 +14,24 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+    }
+
+    class MyAdapter extends FragmentPagerAdapter {
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
     }
 }
